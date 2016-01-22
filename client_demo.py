5f3b2c782f0eb16e97449cd0121e97a399759b5f
Stefan Aleksic <soccerfanatic1996@gmail.com>
2016-01-21 23:25:28 -0500

    i didn't test this and it's python 2 soooo
    but it seems like u have a LEAK

--- client_demo.py
+++ client_demo.py
@@ -1,8 +1,16 @@
 from xbox_api_client import *
 import random
 
-client = XboxAPIClient(open('./auth.txt').read())
-title_ids = open('./title_ids.txt').read().split('\n')
+auths = None
+with open('./auth.txt', 'r') as f:
+	client = f.read()
+	
+client = XboxAPIClient(auths)
+
+title_ids = None
+with open('./title_ids.txt', 'r') as f:
+	title_ids = f.readlines()
+	
 for i in range(0, 8):
 	tid = title_ids[random.randint(0, len(title_ids) -1)]
 	clips = client.get_games_clips_for_title(tid, params={'type': 'UserGenerated'})
